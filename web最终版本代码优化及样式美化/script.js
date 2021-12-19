f0be2b01658f20c89c0c5c7920dcfdd6482cfbe1
ccq12 <81316364+ccq12@users.noreply.github.com>
2021-12-19 13:15:32 +0800

    交互
    加个确认修改的功能，以及提交的历史时间

--- web最终版本代码优化及样式美化/script.js
+++ web最终版本代码优化及样式美化/script.js
@@ -1,7 +1,7 @@
-var time = new Date();
-var year=time.getFullYear(); //获取当前年份
-var month=time.getMonth(); 
-var day=time.getDate(); 
+let time = new Date();
+let year=time.getFullYear(); //获取当前年份
+let month=time.getMonth(); 
+let day=time.getDate(); 
 let date=year+'年'+month+'月'+day+'日';
 
 let todoItems = []; //存放代办事项
@@ -31,11 +31,7 @@ function renderTodoItemList(todoItems, finishedItems) {
 
         inputEl.addEventListener("change", (e) => {
             item.isFinished=true
-            var s=inputEl.nextSibling.innerText+' || '+date
-            if(item.isFinished){
-            item.title=s
-            finishedItems.push(item)}
-            
+            finishedItems.push(item)
             todoItems.splice(i, 1);
             alert('完成了一个任务')
             renderTodoItemList(todoItems, finishedItems)});
@@ -45,14 +41,35 @@ function renderTodoItemList(todoItems, finishedItems) {
         titleEl.contentEditable="true" //让文档可以编辑
         //监听函数，监听到用户修改事项后，监听函数帮我们确定用户改动的文本的位置，然后框内改动后的值修改todoitems，
         //使用innerText插入文本
-        titleEl.addEventListener("input",(e)=>{
-            todoItems[i].title = titleEl.innerText
+        titleEl.addEventListener("mousedown",(e)=>{
+            //文本框监听到鼠标点击
+            let mark=0
+            //设计个标记mark，如果mark=0说明没有被执行过下面的函数，也就是用户还没改动。
+            let sl=todoItems[i].title
+            //存放一下点击的文本框改动前的事项，sl是点击文本框后就立刻保存
+            titleEl.addEventListener("input",(e)=>{
+            //文本框监听到改动，往下执行
+                if(mark==0){ //mark的作用
+                titleEl.addEventListener("mouseleave",(e)=>{
+                //监听鼠标移出文本框事件
+                if(sl!=titleEl.innerText){ //不相等说明用户改动了。
+                if(mark==0 && confirm("确定改动？")){
+                todoItems[i].title = titleEl.innerText;
+                mark=1;  //mark设置成1，说明函数confirm被执行过，防止一直询问
+                renderTodoItemList(todoItems, finishedItems);
+            
+                sl=titleEl.innerText}
+                else{
+                todoItems[i].title=sl; //sl存放一下点击的文本框改动前的事项。
+                renderTodoItemList(todoItems, finishedItems); //重新恢复不修改的样子
+                mark=1; //mark设置成1，说明函数被执行过
+                }}})}})
             //监听器会确定i的值，也就是改动的位置
 
             //下面这行在控制台中打印，帮助调试程序。
-            console.log(todoItems)
             
         })
+        //监听
 
 
         //感叹号打勾字体变红部分
@@ -122,6 +139,11 @@ function renderFinishedItemList(todoItems, finishedItems) {
         let importanceEl = document.createElement("div");
         importanceEl.className = "important-flag"
         importanceEl.innerText = "!";
+
+        let finishedtime = document.createElement("div");
+        finishedtime.className = "finishedtime";        //创建个div
+        finishedtime.innerText = date;
+
         if (item.isImportance) {
             importanceEl.classList.add("open");
             itemDiv.classList.add("important")
@@ -133,7 +155,7 @@ function renderFinishedItemList(todoItems, finishedItems) {
 
         itemDiv.append(titleEl);
         itemDiv.append(importanceEl);
-        
+        itemDiv.append(finishedtime)      //插入创建的包含时间的div
         paneEl.append(itemDiv);
     }
 
@@ -156,7 +178,7 @@ function renderInputPane(todoItems) {
             title: inputEl.value,
             isFinished: false,
             isImportance: false,   })
-        inputEl.value=""  //输入文本后，清空输入框。
+        inputEl.value=""  //输入文本后，清空输入框
         if (hisBtnEl.classList.contains("open")) {
             hisBtnEl.classList.remove("open");}
             
